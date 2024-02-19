7bc2d750cafd197d4146f198d27eedb7c5152360
prisca2920 <prisca2920@gmail.com>
2024-02-19 09:49:55 -0800

    csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""gathering data from an api"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    employeeId = argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+    reply = get(url)
+    userName = reply.json().get('name')
+
+    todoUrl = url + "/todos"
+    reply = get(todoUrl)
+    tasks = reply.json()
+
+    with open('{}.csv'.format(employeeId), 'w') as f:
+        for task in tasks:
+            f.write('"{}","{}","{}","{}"\n'
+                    .format(employeeId, userName, task.get('completed'),
+                            task.get('title')))
