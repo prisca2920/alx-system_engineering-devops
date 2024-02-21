ef01861395ea7824cf71c5f284642bd11473dd0b
prisca2920 <prisca2920@gmail.com>
2024-02-21 01:24:34 -0800

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""exporting to json"""
-import json
+"""fetches information from JSONplaceholder API and exports to JSON"""
+
+from json import dump
 from requests import get
 from sys import argv
 
 
-if __name__ == '__main__':
-    employeeId = argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
-    reply = get(url)
-    username = reply.json().get('name')
+if __name__ == "__main__":
+    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+        argv[1])
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    todoUrl = url + "/todos"
-    reply = get(todoUrl)
-    tasks = reply.json()
+    todo_list = []
+    for todo in todo_result:
+        todo_dict = {}
+        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
+            "completed"), "username": name_result.get("username")})
+        todo_list.append(todo_dict)
 
-    dict = {employeeId: []}
-    for task in tasks:
-        dict[employeeId].append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        })
-    with open('{}.json'.format(employeeId), 'w') as f:
-        json.dump(dict, f)
+    with open("{}.json".format(argv[1]), 'w') as f:
+        dump({argv[1]: todo_list}, f)
