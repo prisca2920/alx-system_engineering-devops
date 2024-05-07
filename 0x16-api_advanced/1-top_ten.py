eb1f16e0dd7ad18d5b85fb93baac3e2c5cc385ff
prisca2920 <prisca2920@gmail.com>
2024-05-07 09:44:28 -0700

    hot 10 posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""titles of the first 10 hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """titles of the first 10 hot posts ."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
