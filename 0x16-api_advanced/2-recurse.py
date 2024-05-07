7ec8f559efcb953e5249e3ea3b3b19851f4c483d
prisca2920 <prisca2920@gmail.com>
2024-05-07 09:55:33 -0700

    recursive

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a list containing the titles of all hot articles"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """a list containing the titles of all hot articles"""
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
+
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
+    else:
+        return None
